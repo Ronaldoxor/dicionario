6a6335ec309d3a7b0128a6f39d21d72181222d2c
Ronaldo <rronaldo.monteiro.almeida@gmail.com>
2025-09-09 09:28:19 -0300

    Inserindo itens e acessando itens em dicionários

--- dicionario.py
+++ dicionario.py
@@ -42,3 +42,40 @@ dicionario3['Melancia'] = 12.00 #inserir novo item
 dicionario3['Abacaxi'] = 10.00 #inserir novo item
 
 print(dicionario3)
+
+#Remover itens
+
+#para remover um item do dicionário, pode-se utilizar a função pop
+#Indicar a chave como parâmetro
+#No caso da chave inexistente, ocorrerá um erro
+
+dicionario4 = {'Banana' : 3.00,
+               'Laranja' : 5.00,
+               'Uva' : 8.00}
+
+#Percorrer valreos do dicionário
+#A estrutura for pode ser utilizada para percorrer os valores(values) e chaves(keys) do dicionário
+
+for n in dicionario4.keys():
+    print(n) #imprime todas as chaves
+
+for n in dicionario4.values():
+    print(n) #imprime todos os valores
+
+for chave,valor in dicionario4.items():
+    print(chave,valor) #imprime todos os itens
+
+dicionario4.pop('Laranja') #Exclui item e chave
+
+#Operador in
+
+#O operador in pode ser utilizado para verificar se uma chave existe no dicionário
+
+dicionario5 = {'Banana' : 3.00,
+               'Laranja' : 5.00,
+               'Uva' : 8.00}
+
+if 'Abacaxi' in dicionario5:
+    print('A chave existe no dicionário')
+else:
+    print('A chave não existe no dicionárioCK')
